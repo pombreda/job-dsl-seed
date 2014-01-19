892f1cd66ccade3e41ffb651dc8160ee43180578
Justin Ryan <jryan@netflix.com>
2014-01-19 00:48:32 -0800

    Move away from Credentials API

--- plugins.dsl.groovy
+++ plugins.dsl.groovy
@@ -1,46 +1,121 @@
-
 // https://github.com/eclipse/egit-github/tree/master/org.eclipse.egit.github.core
 @Grab(group='org.eclipse.mylyn.github', module='org.eclipse.egit.github.core', version='2.1.5')
 import org.eclipse.egit.github.core.*
 import org.eclipse.egit.github.core.client.*
 import org.eclipse.egit.github.core.service.*
 
-def gitHubCredentials = jm.getCredentialsId('NEBULA_PLUGIN_OAUTH')
-
-//OAuth2 token authentication
 GitHubClient client = new GitHubClient()
-client.setOAuth2Token(gitHubCredentials)
+
+def githubProperties = new File(GITHUB_PROPERTIES?:System.getenv()['GITHUB_PROPERTIES'])
+if (githubProperties.exists()) {
+    def props = new Properties()
+    githubProperties.withInputStream { 
+        stream -> props.load(stream) 
+    }
+    def gitHubCredentials = props['github.oauth']
+
+    //OAuth2 token authentication
+    client.setOAuth2Token(gitHubCredentials)
+} else {
+    println "Not providing credentials"
+}
 
 def orgName = 'nebula-plugins'
+def folderName = 'Nebula/'
 
 RepositoryService repoService = new RepositoryService(client);
-def pluginNameRegex = /gradle-(.*)-plugin/
-repoService.getOrgRepositories(orgName).findAll { it.name =~ pluginNameRegex }.each { Repository repo ->
+regex = [/gradle-(.*)-plugin/, /nebula-(\p{Lower}+)$/, /nebula-(.*)-plugin/]
+repoService.getOrgRepositories(orgName).findAll { repo -> regex.any { repo.name =~ it } }.each { Repository repo ->
     def repoName = repo.name
-    def pluginName = (repoName =~ pluginNameRegex)[0][1]
-    println "Plugin Name: ${pluginName}"
+
+    println "Creating jobs for $repoName"
 
     List<RepositoryBranch> branches = repoService.getBranches(repo)
-    branches.collect { RepositoryBranch branch ->
-        job {
-            name "nebula-${pluginName}-snapshot"
-            scm {
-                github("${orgName}/${repoName}", branch.name)
-            }
-            steps {
-                gradle('clean build')
-            }
+    def gradleBranches = branches.findAll { it.name.startsWith('gradle-') }
+    gradleBranches.collect { RepositoryBranch branch ->
+        def nameBase = "${folderName}${repo.name}-${branch.name - 'gradle-'}"
+        snapshot(nameBase, repo.description, orgName, repoName, branch.name )
+        release(nameBase, repo.description, orgName, repoName, branch.name )
+        pullrequest(nameBase, repo.description, orgName, repoName, branch.name )
+    }
+
+    if (gradleBranches.isEmpty()) {
+        // Master
+        def nameBase = "${folderName}${repo.name}-master"
+        snapshot(nameBase, repo.description, orgName, repoName, 'master')
+        release(nameBase, repo.description, orgName, repoName, 'master')
+        pullrequest(nameBase, repo.description, orgName, repoName, 'master' )
+    }
+}
+
+def base(String repoDesc) {
+    job {
+        description ellipsize(repoDesc, 255)
+        logRotator(60,-1,-1,20)
+        wrappers {
+            timeout(20)
         }
-        job {
-            name "nebula-${pluginName}-release"
-            scm {
-                github("${orgName}/${repoName}", branch.name) {
-                    it / userRemoteConfigs / 'hudson.plugins.git.UserRemoteConfig' / credentialsId(gitHubCredentials)
-                }
-            }
-            steps {
-                gradle('clean release')
-            }
+        jdk('Sun JDK 1.6 (latest)')
+        configure { project ->
+            project / 'properties' / 'com.cloudbees.jenkins.plugins.PublicKey'(plugin:'cloudbees-public-key@1.1')
+        }
+        publishers {
+            archiveJunit('**/build/test-results/TEST*.xml')
         }
     }
+}
+
+def release(nameBase, repoDesc, orgName, repoName, branchName) {
+    def job = base(repoDesc)
+    job.with {
+        name "${nameBase}-release"
+        scm {
+            github("${orgName}/${repoName}", branchName, 'git') {
+                //it / userRemoteConfigs / 'hudson.plugins.git.UserRemoteConfig' / credentialsId(gitHubCredentials)
+            }
+        }
+        steps {
+            gradle('clean release')
+        }
+    }
+}
+
+def snapshot(nameBase, repoDesc, orgName, repoName, branchName) {
+    def job = base(repoDesc)
+    job.with {
+        name "${nameBase}-snapshot"
+        scm {
+            github("${orgName}/${repoName}", branchName, 'git')
+        }
+        steps {
+            gradle('clean build') // TODO Upload snapshots to oss.jfrog.org
+        }
+    }
+}
+
+def pullrequest(nameBase, repoDesc, orgName, repoName, branchName) {
+    def job = base(repoDesc)
+    job.with {
+        name "${nameBase}-pull-requests"
+        scm {
+            github("${orgName}/${repoName}", branchName, 'git')
+        }
+        steps {
+            gradle('clean check')
+        }
+        configure { project ->
+            project / triggers / 'com.cloudbees.jenkins.plugins.github__pull.PullRequestBuildTrigger'(plugin:'github-pull-request-build@1.0-beta-2') / spec ()
+            project / 'properties' / 'com.cloudbees.jenkins.plugins.git.vmerge.JobPropertyImpl'(plugin:'git-validated-merge@3.6') / postBuildPushFailureHandler(class:'com.cloudbees.jenkins.plugins.git.vmerge.pbph.PushFailureIsFailure')
+        }
+        publishers {
+            // TODO Put pull request number in build number
+        }
+    }
+}
+
+String ellipsize(String input, int maxLength) {
+  if (input == null || input.length() < maxLength) {
+    return input;
+  }
+  return input.substring(0, maxLength) + '...';
 }
